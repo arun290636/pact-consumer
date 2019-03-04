1569e1183e6061c445edb5ac9ba7e471b77ad48a
arun.kumar <arun.kumar@smartbox.com>
2019-03-04 13:38:05 +0000

    consumer pact

---
+++ src/main/java/com/person/pact/consumer/Person.java
@@ -0,0 +1,16 @@
+package com.person.pact.consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private Integer id;
+    private String name;
+    private String ssn;
+}
---
+++ src/test/java/com/person/pact/consumer/ProviderIntegrationTests.java
@@ -0,0 +1,30 @@
+package com.person.pact.consumer;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProviderIntegrationTests {
+
+	@Autowired
+	private ProviderClient providerClient;
+
+	@Test
+	@Ignore
+	public void createNewPerson() {
+		Person person = Person.builder().name("Harald Hårfagre").ssn("01039012345").build();
+
+		Person response = providerClient.createPerson(person);
+
+		assertEquals(response.getName(), person.getName());
+		assertEquals(response.getSsn(), person.getSsn());
+		assertNotNull(response.getId());
+	}
+}
